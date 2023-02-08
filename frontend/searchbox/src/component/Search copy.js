1ee2a4a6e4b51f263f79f15e855a0d6d6d10b897
Praba31-1993 <prabamymotowd@gmail.com>
2023-02-08 20:42:50 +0530

    searchbox style with border Style

---
+++ frontend/searchbox/src/component/Search copy.js
@@ -0,0 +1,45 @@
+import React from "react";
+import "./Search.css"
+import VoicePic from "../assets/Voice.png"
+
+function Search() {
+  const Styles = {
+    margin: "0 auto",
+    padding: "10px",
+    width: "50%",
+
+    border: "1px solid black",
+    display: "flex",
+    top: "3em",
+
+  };
+  
+  const inputStyles ={
+    borderRadius: "15px"
+  }
+
+  return (
+    <div className="SearchBody">
+     <h1 style={{fontSize:"14em"}}><strong><span ><span class="g-blue">G</span><span
+        class="o-red">o</span><span class="o-yellow">o</span><span class="g-blue">g</span><span
+        class="l-green">l</span><span class="o-red e-red">e</span></span></strong></h1>
+      <div className="input-group mb-3" style={Styles}>
+        <input
+          type="text"
+          className="form-control"
+          placeholder="Search"
+          aria-label="Recipient's username"
+          aria-describedby="basic-addon2"
+          style={inputStyles}
+        />
+        <div className="input-group-append">
+          
+            <img src={VoicePic} alt="voice picture" width={50} height={50}/>
+          
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Search;
--- frontend/searchbox/src/component/Search.js
+++ frontend/searchbox/src/component/Search.js
@@ -1,6 +1,6 @@
 import React from "react";
-import "./Search.css"
-import VoicePic from "../assets/Voice.png"
+import "./Search.css";
+import VoicePic from "../assets/Voice.png";
 
 function Search() {
   const Styles = {
@@ -8,21 +8,32 @@ function Search() {
     padding: "10px",
     width: "50%",
 
-    border: "1px solid black",
+    // border: "1px solid black",
     display: "flex",
     top: "3em",
-
+    borderRadius: "50px",
+    background: "#e0e0e0",
+    boxShadow: "20px 20px 60px #bebebe, -20px -20px 60px #ffffff",
   };
-  
-  const inputStyles ={
-    borderRadius: "15px"
-  }
+
+  const inputStyles = {
+    borderRadius: "15px",
+  };
 
   return (
     <div className="SearchBody">
-     <h1 style={{fontSize:"14em"}}><strong><span ><span class="g-blue">G</span><span
-        class="o-red">o</span><span class="o-yellow">o</span><span class="g-blue">g</span><span
-        class="l-green">l</span><span class="o-red e-red">e</span></span></strong></h1>
+      <h1 style={{ fontSize: "14em" }}>
+        <strong>
+          <span>
+            <span class="g-blue">G</span>
+            <span class="o-red">o</span>
+            <span class="o-yellow">o</span>
+            <span class="g-blue">g</span>
+            <span class="l-green">l</span>
+            <span class="o-red e-red">e</span>
+          </span>
+        </strong>
+      </h1>
       <div className="input-group mb-3" style={Styles}>
         <input
           type="text"
@@ -33,9 +44,7 @@ function Search() {
           style={inputStyles}
         />
         <div className="input-group-append">
-          
-            <img src={VoicePic} alt="voice picture" width={50} height={50}/>
-          
+          <img src={VoicePic} alt="voice picture" width={50} height={50} />
         </div>
       </div>
     </div>
